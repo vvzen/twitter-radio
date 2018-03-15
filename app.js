b0d0e488103a9b5f112c364edadd33488d8b9715
Valerio Viperino <vv@Valerios-Air.eduroam.student>
2018-03-15 14:09:26 +0000

    working on user input

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const fs = require('fs');
 const Twit = require('twit');
 const say = require('say');
 
-var listening_words = ["generative art", "art", "facebook"];
 var voices = {
     "en" : ["Alex", "Karen", "Fred", "Moira"],
     "it" : ["Luca", "Alice"]
 };
 
-var twitter_auth = JSON.parse(fs.readFileSync("auth.json"));
+const twitter_auth = JSON.parse(fs.readFileSync("auth.json"));
 
-var T = new Twit({
+// Twit object for accessing twitter api
+const T = new Twit({
     consumer_key: twitter_auth.consumer_key,
     consumer_secret: twitter_auth.consumer_secret,
     access_token: twitter_auth.access_token,
@@ -18,38 +18,75 @@ var T = new Twit({
     timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
 });
 
-// Create a stream object that filters the public stream
-var stream = T.stream("statuses/filter", {
-    track: listening_words,
-    language: "en"
-});
+// for reading user input
+var stdin = process.stdin;
 
-console.log(`started streaming on ${listening_words.toString()}`);
+// resume stdin in the parent process (node app won't quit all by itself
+// unless an error or process.exit() happens)
+stdin.resume();
+stdin.setRawMode(true);
+stdin.setEncoding("utf8"); // i don't want binary, do you?
 
-say.stop();
+// on any data into stdin
+var current_keywords = "";
+console.log("What words should we listen to?");
+stdin.on("keypress", (letter, key) => {
 
-// Start streaming
-stream.on("tweet", function (tweet) {
-    // console.log(tweet.text);
-
-    // remove the https string from the text using a regex
-    var text_cleaned = tweet.text.split(/https:\/\/\w+.co\/\w+/);
-
-    if (text_cleaned.length > 0){
+    // console.log(key);
+    // console.log(`${letter}`);
+    //console.log(`${current_keywords}`);
+    
+    // on ctrl-c
+    if (key.sequence === '\u0003'){
+        console.log("exiting..");
+        say.stop();
+        process.exit();
+    }
+    // on enter press
+    else if (key.sequence === '\r'){
+        console.log(`started streaming on ${current_keywords}\n`);
         
-        //say.stop();
-        stream.stop();
-
-        let current_voice = voices.en[Math.floor(Math.random()*voices.en.length)];
-
-        say.speak(text_cleaned[0], current_voice, 1.1, (err) => {
-            
-            if (err){
-                console.log(err);
-            }
-
-            stream.start();
+        say.stop();
+        
+        // Create a stream object that filters the public stream
+        var stream = T.stream("statuses/filter", {
+            track: current_keywords,
+            language: "en"
         });
-        console.log("heard a new tweet!");
+
+        // Start streaming
+        stream.on("tweet", (tweet) => {
+            // console.log(tweet.text);
+
+            // remove the https string from the text using a regex
+            var text_cleaned = tweet.text.split(/https:\/\/\w+.co\/\w+/);
+
+            if (text_cleaned.length > 0){
+                
+                say.stop();
+                stream.stop();
+
+                let current_voice = voices.en[Math.floor(Math.random()*voices.en.length)];
+
+                say.speak(text_cleaned[0], current_voice, 1.1, (err) => {
+                    
+                    if (err){
+                        console.log(err);
+                    }
+                    else {
+                        stream.start();
+                    }
+                });
+                console.log("heard a new tweet!");
+            }
+        });
+        current_keywords = "";
+    }
+    // on backspace, remove last letter
+    else if (key.name === 'backspace'){
+        current_keywords = current_keywords.slice(0, -1);
+    }
+    else {
+        current_keywords += letter;
     }
 });
